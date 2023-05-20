2d58c54aa1a272ffb068b25dea1b39e01f54ea12
bagusuntoro <untorobagus65@gmail.com>
2023-05-20 13:33:23 +0700

    terkena error pengalihan

--- lib/VoteScreen.dart
+++ lib/VoteScreen.dart
@@ -90,8 +90,9 @@ class VoteScreen extends StatelessWidget {
                                       child: const Text("Batal"),
                                     ),
                                     TextButton(
-                                      onPressed: () {
-                                        voteCandidate(context, candidate.id); //
+                                      onPressed: () async {
+                                        await voteCandidate(context,
+                                            int.parse(candidate.id)); //
                                         Navigator.of(context).pop();
                                       },
                                       child: const Text("Vote"),
@@ -124,7 +125,7 @@ class VoteScreen extends StatelessWidget {
     );
   }
 
-  Future<void> voteCandidate(BuildContext context, String candidateId) async {
+  Future<void> voteCandidate(BuildContext context, int candidateId) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
 
@@ -133,66 +134,79 @@ class VoteScreen extends StatelessWidget {
       'Content-Type': 'application/json',
     };
 
-    var userResponse = await http.get(
+    http.Response userResponse = await http.get(
       Uri.parse('http://localhost:8000/api/auth/me'),
       headers: headers,
     );
 
     if (userResponse.statusCode == 200) {
-      final userData = jsonDecode(userResponse.body);
-      final dataUser = userData['data'];
-      String userId = dataUser['id'];
+      // Berhasil mendapatkan data pengguna
+      var userData = json.decode(userResponse.body);
+      int userId = userData['id'];
 
-
-      var response = await http.post(
+      // String? token = prefs.getString('token');
+      Map<String, String> headers = {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+      http
+          .post(
         Uri.parse('http://localhost:8000/api/auth/votes'),
-        body: jsonEncode({
-          'voter_id': userId,
-          'candidate_id': candidateId,
-        }),
         headers: headers,
-      );
-
-      if (response.statusCode == 200) {
-        // Berhasil memilih
-        showDialog(
-          context: context,
-          builder: (BuildContext dialogContext) {
-            return AlertDialog(
-              title: const Text("Vote Successful"),
-              content: const Text("Anda telah berhasil memilih kandidat."),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(dialogContext).pop();
-                    // Lakukan navigasi ke layar lain jika diperlukan
-                  },
-                  child: const Text("OK"),
+        body: json.encode({
+          'candidate_id': candidateId,
+          'user_id': userId,
+        }),
+      )
+          .then((voteResponse) {
+        if (voteResponse.statusCode == 200) {
+          // Berhasil memilih
+          showDialog(
+            context: context,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                title: const Text("Vote Successful"),
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Nama: ${userData['name']}"),
+                    Text("Email: ${userData['email']}"),
+                  ],
                 ),
-              ],
-            );
-          },
-        );
-      } else {
-        // Gagal memilih
-        showDialog(
-          context: context,
-          builder: (BuildContext dialogContext) {
-            return AlertDialog(
-              title: const Text("Vote Failed"),
-              content: Text("Gagal memilih. Silakan coba lagi."),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(dialogContext).pop();
-                  },
-                  child: const Text("OK"),
-                ),
-              ],
-            );
-          },
-        );
-      }
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                      // Lakukan navigasi ke layar lain jika diperlukan
+                    },
+                    child: const Text("OK"),
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // Gagal memilih
+          showDialog(
+            context: context,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                title: const Text("Vote Failed"),
+                content: Text("Gagal memilih. Silakan coba lagi."),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(dialogContext).pop();
+                    },
+                    child: const Text("OK"),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      });
     } else {
       // Gagal mendapatkan data pengguna
       showDialog(
